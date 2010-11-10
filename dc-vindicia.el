af39662a1ce819285a34b3bf441830bade100f54
Donnie Cameron <root@donnieknows.com>
2010-11-10 12:32:50 -0800

    Improved jira-diff function

--- dc-vindicia.el
+++ dc-vindicia.el
@@ -1,12 +1,13 @@
 ;; This is dc-vindicia.el, routines to help out with work at Vindicia
 
-(defun jira-diff (revision)
-  (let ((prev-revision (1- revision)))
+;; The repo parameter is optional and should be "vindicia" or
+;; "vindicia-www" or something like that
+(defun jira-diff (revision &optional (repo "vindicia"))
+  (let ((prev-revision (1- revision))
+        (svn-uri (concat "https://svn.vindicia.com/svn/" repo)))
     (with-current-buffer (or (get-buffer "jira-diff")
                              (generate-new-buffer "jira-diff"))
       (erase-buffer)
-      (insert
-       (shell-command-to-string
-        (format "svn diff -r %s:%s https://svn.vindicia.com/svn/vindicia"
-                prev-revision revision)))
+      (insert (shell-command-to-string
+               (format "svn diff -r %s:%s %s" prev-revision revision svn-uri)))
       (switch-to-buffer (current-buffer)))))
