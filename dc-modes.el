0df07d9e1e32c321a2c39018b578f985ee472c83
Donnie Cameron <root@donnieknows.com>
2013-03-17 02:44:46 -0700

    Added haskell-mode

--- dc-modes.el
+++ dc-modes.el
@@ -249,59 +249,9 @@ Null prefix argument turns off the mode."
 ;; Temmplate Toolkit files should be opened in HTML mode
 (add-to-list 'auto-mode-alist '("\\.tt2" . html-mode))
 
-;; ;;
-;; ;; BEGIN Haskell Setup
-;; ;;
-
-;; ;; Font Locking, Programming Modes, and Compilation settings
-;; ;;
-
-;; ;; maximum colors
-;; (setq font-lock-maximum-decoration t)
-
-;; ;; extra key bindings
-;; (global-set-key "\M-C" 'compile)
-;; (global-set-key "\C-^" 'next-error)
-;; (global-set-key "\C-\M-g" 'goto-line)
-
-;; ;; use spaces instead of tabs
-;; (setq indent-tab-mode nil)
-
-;; ;; haskell mode configuration
-;; (setq auto-mode-alist
-;;       (append auto-mode-alist
-;;               '(("\\.[hg]s$"  . haskell-mode)
-;;                 ("\\.hic?$"     . haskell-mode)
-;;                 ("\\.hsc$"     . haskell-mode)
-;;                 ("\\.chs$"    . haskell-mode)
-;;                 ("\\.l[hg]s$" . literate-haskell-mode))))
-;; (autoload 'haskell-mode "haskell-mode"
-;;   "Major mode for editing Haskell scripts." t)
-;; (autoload 'literate-haskell-mode "haskell-mode"
-;;   "Major mode for editing literate Haskell scripts." t)
-
-;; ;adding the following lines according to which modules you want to use:
-;; (require 'inf-haskell)
-
-;; (add-hook 'haskell-mode-hook 'turn-on-font-lock)
-;; ;(add-hook 'haskell-mode-hook 'turn-off-haskell-decl-scan)
-;; ;(add-hook 'haskell-mode-hook 'turn-off-haskell-doc-mode)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;; ;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-;; ;(add-hook 'haskell-mode-hook 'turn-on-haskell-hugs)
-;; ;(add-hook 'haskell-mode-hook 'turn-on-haskell-ghci)
-;; ;(add-hook 'haskell-mode-hook 
-;; ;   (function
-;; ;    (lambda ()
-;; ;      (setq haskell-program-name "ghci")
-;; ;      (setq haskell-ghci-program-name "ghci6")
-;; ;      (setq haskell-ghci-program-args 
-;; ;         '("-fcontext-stack=30" 
-;; ;           "-fglasgow-exts" 
-;; ;           "-farrows")))))
-
-;; ;;
-;; ;; END Haskell Setup
-;; ;;
-
-
+;; For Haskell
+(load "/usr/share/emacs24/site-lisp/haskell-mode/haskell-mode.elc")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
